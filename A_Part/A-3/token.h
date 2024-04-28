025fd46c988d06aff2b4977ed9a24afd6186eb76
Aggelos Mentzelos <filegeiasou@gmail.com>
2024-04-28 13:28:12 +0300

    Add files via upload

--- A_Part/A-3/token.h
+++ A_Part/A-3/token.h
@@ -1,8 +1,8 @@
 #define DELIMITER 1
 #define NUMBERS 2
-#define VARIABLE 3
-#define IDENTIFIERS 4
-#define STRINGS 5
-#define COMMENTS 6
-#define OPERATORS 7
-#define KEYWORD 8
+#define STRINGS 3
+#define OPERATORS 4
+#define IDENTIFIERS 5
+#define VARIABLE 6
+#define COMMENTS 7
+#define KEYWORD 9
