4c265f99cd26c91d64691d361d822adc9ada4e4e
filegeiasou <filegeiasou@gmail.com>
2024-06-16 11:41:51 +0300

    input file b3

---
+++ B_Part/B-3/a.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int i;
+    for(i=0 ; i<5;  i++ %)
+    {
+        printf("%d",a);
+    }
+    int b = 20;
+    int c = a +  ;
+    return 0;
+}
